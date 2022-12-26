ed2bfbe1d4f28011396c0e58370cc8c4966b77ba
Tuan Nguyen <ntuan.2502@gmail.com>
2022-12-26 09:21:38 +0700

    update database config

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   connection: {
-    client: "postgres",
+    client: env("DATABASE_TYPE"),
     connection: {
       host: env("DATABASE_HOST"),
       port: env.int("DATABASE_PORT"),
@@ -8,9 +8,6 @@ module.exports = ({ env }) => ({
       user: env("DATABASE_USERNAME"),
       password: env("DATABASE_PASSWORD"),
       ssl: env.bool("DATABASE_SSL"),
-    },
-    pool: {
-      max: 5,
     },
   },
 });
